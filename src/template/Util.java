49d4556d56c2dd8b54ad2269292c2723e4d2a5cb
Rob Hausch <rob.hausch@gmail.com>
2017-01-09 21:51:34 -0800

    Added template for creating new bots

---
+++ src/template/Util.java
@@ -0,0 +1,10 @@
+package template;
+
+import battlecode.common.BulletInfo;
+import battlecode.common.Direction;
+import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
+
+public class Util extends Globals {
+
+}
